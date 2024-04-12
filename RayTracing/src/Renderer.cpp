597dbdc2c42f1a6513b8025ca5b4bcf837047002
Antônio Chaves <antoniovasconcelloschaves@gmail.com>
2024-04-12 12:00:04 -0300

    Add shading from the 5th episode

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,7 +1,19 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace Utils
+{
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
 
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -29,7 +41,10 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { ((float)x / (float)m_FinalImage->GetWidth()), ((float)y / (float)m_FinalImage->GetHeight()) };
 			coord = coord * 2.0f - 1.0f; // -1 -> 1
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
@@ -37,7 +52,7 @@ void Renderer::Render()
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
@@ -52,8 +67,23 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 
 	float discriminant = b * b - 4.0f * a * c;
 
-	if (discriminant >= 0.0f)
-		return 0xffff00ff;
+	if (discriminant < 0.0f)
+		return glm::vec4(0, 0, 0, 1);
 
-	return 0xff000000;
+	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	//glm::vec3 h0 = rayOrigin + rayDirection * t0;
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+
+	glm::vec3 normal = glm::normalize(hitPoint);
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+
+	float d = glm::max(glm::dot(normal, -lightDir), 0.0f); // == cos(alngulo entre eles)
+
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= d;
+
+	return glm::vec4(sphereColor, 1.0f);
 }
